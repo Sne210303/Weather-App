59429028a00facc5c31b215ffa08c36535b4302a
Sne210303 <snenhlanhlamkhwanazi24@gmail.com>
2023-12-06 13:15:49 +0200

    Added Basic JS content and removed the h1 content along with the temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "fe2oe4at2732008d649aee0afa9012b3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} &units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("search-form-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-}
+  let searchFormElement = document.querySelector("#search-form");
+  searchCity(searchInput.value);
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  searchCity("Paris");
+}
