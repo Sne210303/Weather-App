71f2bc18eaad8a735b4ffc1ce5bfe689a3817b38
Sne210303 <snenhlanhlamkhwanazi24@gmail.com>
2023-12-06 12:57:00 +0200

    Added axios into in HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
 
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
